c2202d56ac0789d45305090ec08edecb6beabaf5
Kelvin Vu <kelvn.developer@gmail.com>
2025-04-11 17:56:51 +0700

    init project

---
+++ src/test/java/com/meikocn/api/MeikocnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meikocn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeikocnApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
